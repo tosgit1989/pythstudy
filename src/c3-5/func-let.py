ddd70f8d15b4b93ca912feb41381c9e03e8287ff
tosgit1989 <purannaa1989@gmail.com>
2016-12-03 06:04:55 +0900

    無名関数を用いたプログラムを作成

---
+++ src/c3-5/func-let.py
@@ -0,0 +1,13 @@
+def mul_func(a, b):
+  return a * b
+
+def div_func(a, b):
+  return a / b
+
+func = mul_func
+resuit = func(2, 3)
+print(resuit)
+
+func2 = div_func
+resuit = func2(10, 5)
+print(resuit)
