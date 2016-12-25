9315ad3c8d1f089a25581037e192c5af5db43f8c
tosgit1989 <purannaa1989@gmail.com>
2016-12-25 15:41:50 +0900

    サイコロのWebアプリを作成

---
+++ src/c5-2/cgi-bin/saikoro.py
@@ -0,0 +1,14 @@
+import random
+
+print("Content-Type: text/html")
+print("")
+
+no = random.randint(1, 6)
+print("""
+  <html>
+  <head><title>Dice</title></head>
+  <body>
+    <h1>{num}</h1>
+  </body>
+  </html>
+""".format(num=no))
