9572600767f0a1a608d28355c9194a27e3353783
tosgit1989 <purannaa1989@gmail.com>
2017-01-07 05:31:35 +0900

    基底クラスと派生クラスとで、同名のメソッドを定義した場合、どちらのメソッドが実行されるかを検証

---
+++ src/c6-2/override-test.py
@@ -0,0 +1,10 @@
+class SuperClass:
+    def hoge(self):
+        print("SuperClass.hoge")
+
+class SubClass(SuperClass):
+    def hoge(self):
+        print("SubClass.hoge")
+
+it = SubClass()
+it.hoge()
