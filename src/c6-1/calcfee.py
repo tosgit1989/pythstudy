513566f03bb2598886788e3b683557b1155e9e32
tosgit1989 <purannaa1989@gmail.com>
2017-01-07 00:57:08 +0900

    商品価格を、送料込み・税込みで計算するプログラムを作成

---
+++ src/c6-1/calcfee.py
@@ -0,0 +1,29 @@
+import math
+
+class CalcFee:
+  def __init__(self):
+    ''' 初期化処理 '''
+    self.shipping_fee = 1000
+    self.tax_rate = 0.08
+    self.value = 0
+
+  def addItem(self, price):
+    ''' 商品の値段を加算する '''
+    self.value += price
+
+  def calc(self):
+    ''' 最終的な料金を計算する '''
+    total = self.value + self.shipping_fee
+    tax = math.ceil(total * self.tax_rate)
+    v = math.ceil(total + tax)
+    return v
+
+fee1 = CalcFee()
+fee1.addItem(500)
+print(fee1.calc(), "円")
+
+fee2 = CalcFee()
+fee2.shipping_fee = 1500
+fee2.addItem(800)
+fee2.addItem(500)
+print(fee2.calc(), "円")
